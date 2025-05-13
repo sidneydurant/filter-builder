6c8a60d071bf54daf2226b1c6e358090e51a359d
sidney durant <sidney.durant@gmail.com>
2025-05-13 14:41:01 -0700

    initial commit

---
+++ vitest.config.js
@@ -0,0 +1,10 @@
+// vitest.config.js or vitest.config.ts
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'], // Optional: path to your setup file
+  },
+});
